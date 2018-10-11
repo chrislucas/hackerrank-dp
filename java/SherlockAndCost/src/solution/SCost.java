aff6ff78ed1ddfbf6f5699bb98c3355311cf20b5
chrislucas <christoffer.luccas@gmail.com>
2018-10-11 17:20:08 -0300

    um problema para resolver

---
+++ java/SherlockAndCost/src/solution/SCost.java
@@ -0,0 +1,12 @@
+package solution;
+
+
+/**
+ * https://www.hackerrank.com/challenges/sherlock-and-cost/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * */
+public class SCost {
+
+    public static void main(String[] args) {
+
+    }
+}
